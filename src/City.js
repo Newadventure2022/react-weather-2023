67431b6c5f19af556d0ab4f204de0e89307d8a72
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-03-17 00:39:33 +0100

    Added new files for the city

---
+++ src/City.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+import "./City.css";
+export default function City() {
+  function handleResponse(response) {
+    alert(`The weather in Valencia is ${response.data.main.temp}°C`);
+  }
+  let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Valencia&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h1>Hello from Citys </h1>;
+}
