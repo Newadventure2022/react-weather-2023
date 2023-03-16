85e52f0c81ffc52efc1e8c78c908fede5c307c16
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-03-16 16:01:02 +0100

    Testing background image

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <button className="btn btn-primary">Hello!</button>
-      </header>
-      <Weather />
-      <Footer />
+      <div className="container">
+        <header className="App-header">
+          <button className="btn btn-primary">Hello!</button>
+        </header>
+        <Weather />
+        <p>
+          Valencia was founded as a Roman colony in 138 BC. Islamic rule and
+          acculturation ensued in the 8th century, together with the
+          introduction of new irrigation systems and crops. Aragonese Christian
+          conquest took place in 1238, and so the city became the capital of the
+          Kingdom of Valencia.
+        </p>
+        <Footer />
+      </div>
     </div>
   );
 }
