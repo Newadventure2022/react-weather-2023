786cadc8c211e42a6c2f8fe52d1eb32a008a10ce
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-03-26 06:27:56 +0200

    Updated the app colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Weather from "./Weather";
-
 import Footer from "./Footer";
 import "./App.css";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <div className="backgroundColor">
+      <div className="backgroundColor ">
         <div className="container">
           <Weather defaultCity="Valencia" />{" "}
         </div>
