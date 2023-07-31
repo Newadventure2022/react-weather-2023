77a4328fa98104f37dde11ef21932119b4773cb4
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-31 17:52:50 +0200

    Updated the units, footer, forecast days, and form

--- src/Footer.js
+++ src/Footer.js
@@ -5,19 +5,30 @@ import logo from "./logo.svg";
 export default function Footer() {
   return (
     <div className="Footer">
+      Created by
       <a
-        href="https://github.com/Newadventure2022"
+        href="https://celia-corona-doran2023.netlify.app/"
         target="_blank"
         rel="noreferrer"
         className="Visit"
       >
-        Created <span className="my-name"> by Celia Corona-Doran</span> with
-        React{" "}
-        <span>
-          {" "}
-          <img src={logo} className="App-logo" alt="logo" />
-        </span>
-        and open-sourced on <span className="github">GitHub</span>
+        <span className="my-name"> Celia Corona-Doran</span>{" "}
+      </a>{" "}
+      with React{" "}
+      <span>
+        {" "}
+        <img src={logo} className="App-logo" alt="logo" />
+      </span>
+      and open-sourced on{" "}
+      <a
+        href="https://github.com/Newadventure2022/react-weather-2023"
+        target="_blank"
+        rel="noreferrer"
+        title="Link to Celia's GitHub"
+      >
+        {" "}
+        <span className="github">GitHub</span>.
+        <i className="fa-brands fa-github"></i>
       </a>
     </div>
   );
